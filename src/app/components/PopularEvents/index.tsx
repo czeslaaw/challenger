b72fd8a084a6946d34c05db02b71922cfcd0cbed
krzysztofczech <krzysztof.czech2@gmail.com>
2025-08-22 17:53:44 +0200

    chore: a11y fixes for PopularEvents + dark mode fixes

--- src/app/components/PopularEvents/index.tsx
+++ src/app/components/PopularEvents/index.tsx
@@ -14,24 +14,21 @@ export default async function PopularEvents() {
 
       <ul className="list-none grid sm:grid-cols-2 md:grid-cols-3 gap-3">
         {events.map((event) => (
-          <li key={event.id} className="relative aspect-video rounded-lg overflow-hidden">
-            <div className="absolute inset-x-0 bottom-0 p-2">
-              <h1 className="text-sm text-primary-foreground">{event.name}</h1>
-              <p className="text-xs text-secondary-foreground">
-                {event.location.name}
-                {" - "}
-                {new Date(event.date).toLocaleDateString()}
-              </p>
-            </div>
-
+          <li key={event.id} className="flex flex-col gap-1">
             <Image
               role="presentation"
-              className="object-cover h-full w-full"
+              className="object-cover h-full w-full aspect-video rounded-lg"
               alt={event.name}
               src={event.imageUrl}
               width={320}
               height={200}
             />
+            <p className="text-sm text-foreground dark:text-primary-foreground">{event.name}</p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              {event.location.name}
+              {" - "}
+              {new Date(event.date).toLocaleDateString()}
+            </p>
           </li>
         ))}
       </ul>
