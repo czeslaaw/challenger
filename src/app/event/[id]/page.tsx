1c152f60899d8c49eaf5a1927970036e7abccee6
krzysztofczech <krzysztof.czech2@gmail.com>
2025-08-25 13:44:24 +0200

    bug: add semantics on event page

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -16,42 +16,45 @@ export default async function EventPage({ params }: EventPageProps) {
   }
 
   return (
-    <>
-      <h1 className="text-3xl font-bold mb-4">{event.name}</h1>
-      <Image src={event.imageUrl} alt={event.name} width={600} height={400} className="rounded-lg w-full" />
-      <div className="flex flex-col gap-4">
-        <p className="text-gray-600 dark:text-gray-200">{event.description}</p>
-        <div className="flex gap-2 items-center">
-          <Calendar size={20} />
-          <span className="font-semibold">Date:</span>
-          <span>{new Date(event.date).toLocaleDateString()}</span>
-        </div>
-      </div>
-
-      <h2 className="flex items-center gap-2 text-2xl font-semibold mt-6 mb-2">
-        <Map />
-        <span>Location</span>
-      </h2>
-      <div className="grid sm:grid-cols-2 gap-4">
-        <Image
-          src={event.location.imageUrl}
-          alt={event.location.name}
-          width={600}
-          height={400}
-          className="grid-span-2 rounded-lg w-full"
-          unoptimized
-        />
+    <article>
+      <section>
+        <h1 className="text-3xl font-bold mb-4">{event.name}</h1>
+        <Image src={event.imageUrl} alt={event.name} width={600} height={400} className="rounded-lg w-full" />
         <div className="flex flex-col gap-4">
-          <p className="text-xl text-black dark:text-gray-200">{event.location.name}</p>
-          <div className="flex gap-2">
-            <MapPin size={20} />
-            <span className="font-semibold">Address:</span>
-            <span>
-              {event.location.city}, {event.location.country}
-            </span>
+          <p className="text-gray-600 dark:text-gray-200">{event.description}</p>
+          <div className="flex gap-2 items-center">
+            <Calendar size={20} />
+            <span className="font-semibold">Date:</span>
+            <span>{new Date(event.date).toLocaleDateString()}</span>
           </div>
         </div>
-      </div>
-    </>
+      </section>
+      <section>
+        <h2 className="flex items-center gap-2 text-2xl font-semibold mt-6 mb-2">
+          <Map />
+          <span>Location</span>
+        </h2>
+        <div className="grid sm:grid-cols-2 gap-4">
+          <Image
+            src={event.location.imageUrl}
+            alt={event.location.name}
+            width={600}
+            height={400}
+            className="grid-span-2 rounded-lg w-full"
+            unoptimized
+          />
+          <div className="flex flex-col gap-4">
+            <p className="text-xl text-black dark:text-gray-200">{event.location.name}</p>
+            <address className="flex gap-2">
+              <MapPin size={20} />
+              <span className="font-semibold not-italic">Address:</span>
+              <span>
+                {event.location.city}, {event.location.country}
+              </span>
+            </address>
+          </div>
+        </div>
+      </section>
+    </article>
   )
 }
